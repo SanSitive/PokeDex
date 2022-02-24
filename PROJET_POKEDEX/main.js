82f04ae983d2e766ad539a5f9c9d81fa8ac0d879
Maxime <max341@free.fr>
2022-02-24 18:52:01 +0100

    Pokemon V2

--- PROJET_POKEDEX/main.js
+++ PROJET_POKEDEX/main.js
@@ -1,9 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            details:[],
-            popup:{id:1,name:'Bulbasaur',image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',stats:''},
-            popupPositions:{x:'45%',y:100},
+            details:[]/*Stocker les pokémons */,
+            popup:{id:1,name:'Bulbasaur',image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',stats:''},/*Stocker les stats à afficher dans la popup pokémon*/
+            popupPositions:{x:'45%',y:100},/*Stocker la position optimale basique de la popup */
         }
     },
     methods:{
@@ -11,7 +11,7 @@ const app = Vue.createApp({
             /* Pour améliorer : récupérer la liste de tout les pokémon que l'on peut appeler via l'API Pokemon */
             /* puis faire un fetch pour chacun de ces pokémons pour éviter de chercher manuellement le nombre max de pokémon */
             if (!this.details.length){
-                for(let i =1; i<151; i++){
+                for(let i =1; i<899; i++){
                     fetch('https://pokeapi.co/api/v2/pokemon/'+i).then(resp=> resp.json()).then(data => {
                         let temporary = {id:0 ,name:'',image:'',stats:''};
                         temporary.id = data.id;
@@ -30,6 +30,7 @@ const app = Vue.createApp({
             }
         },
         displayPopup(id){
+            /*Afficher la popup*/
             let index = 0;
             while(this.details[index].id != id ){
                 index++;
@@ -45,10 +46,12 @@ const app = Vue.createApp({
             pop.setAttribute('open',true);
         },
         hidePopup(){
+            /* Cacher la vue détaillé du pokémon*/
             let pop = document.getElementById("popup");
             pop.removeAttribute('open');
         },
         sortByName(){
+            /*Trier le tableau contenant les pokémons par Nom */
             this.details.sort(function(a,b){
                 if (a.name < b.name){
                     return -1;
@@ -60,11 +63,13 @@ const app = Vue.createApp({
             })
         },
         sortById(){
+            /*Trier le tableau contenant les pokémons par Id */
             this.details.sort(function(a,b){
                 return a.id - b.id;
             })
         },
         request(){
+            /*Récupérer les données du form pour rediriger la recherche en fonction du type de recherche souhaité */
             let req = {field:'',selected:''};
             req.field = document.getElementById('field').value;
             req.selected = document.getElementById('type-search').options[document.getElementById('type-search').selectedIndex].text;
@@ -77,6 +82,7 @@ const app = Vue.createApp({
             }
         },
         searchByName(name){
+            /*Recherche par Nom d'un pokémon dans le tableau contenant les pokémons */
             let tab = document.getElementsByClassName('Pokemon-Tiles-Item');
             for(let i=0; i<tab.length; i++){
                 if (tab[i].innerHTML.search(name) == -1 ){
@@ -85,6 +91,7 @@ const app = Vue.createApp({
             }
         },
         searchById(id){
+            /*Recherche par Id d'un pokémon dans le tableau contenant les pokémons */
             let tab = document.getElementsByClassName('Pokemon-Tiles-Item');
             for(let i=0; i<tab.length; i++){
                 if (parseInt(tab[i].id) != parseInt(id) ){
@@ -93,12 +100,14 @@ const app = Vue.createApp({
             }
         },
         showAll(){
+            /*Affiche tout les pokémons*/
             let tab = document.getElementsByClassName('Pokemon-Tiles-Item');
             for(let i=0; i<tab.length; i++){
                     tab[i].style.display = 'block';
             }
         }
     },mounted(){
+        /* Les fonctions à lancer dès le démarrage de l'app */
         this.pokemonDataRequest();
         document.getElementById('form').addEventListener("click",function(event){
             event.preventDefault();
